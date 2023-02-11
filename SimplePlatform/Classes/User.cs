22ab05893db272987f58959a8abce045c4ab8ba0
VyacheslavZinch <89160613+VyacheslavZinch@users.noreply.github.com>
2023-02-12 00:06:59 +0200

    2/12/2023

--- SimplePlatform/Classes/User.cs
+++ SimplePlatform/Classes/User.cs
@@ -2,11 +2,29 @@
 using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc;
+using Server;
 
 namespace SimplePlatform.Classes
 {
-    public class User
+    internal class User
     {
+        private readonly string Name;
+        private readonly string? Surname;
+        private readonly string Email;
+        private readonly DateTime DateOfRegistration;
+        private readonly string Password;
+        private readonly Guid UID;
 
+#pragma warning disable CS8618 
+        public User(string name, string? surname, string email, string password, string confirmpassword)
+#pragma warning restore CS8618 
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Email = email;
+            this.DateOfRegistration = DateTime.Now;
+            if (password.ToString() == confirmpassword.ToString()) this.Password = Crypto.HashPassword(password);
+            this.UID = System.Guid.NewGuid();
+        }
     }
 }
--- SimplePlatform/Program.cs
+++ SimplePlatform/Program.cs
@@ -2,16 +2,13 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -26,7 +23,4 @@ app.MapRazorPages();
 
 app.Run();
 
-//Scaffold-DbContext 'Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=GeneralDB;Trusted_Connection=True;' Microsoft.EntityFrameworkCore.SqlServer
-
-//DESKTOP-5R0C0SM\SQLEXPRESS
 //Scaffold-DbContext 'Data Source=DESKTOP-5R0C0SM\SQLEXPRESS;Initial Catalog=GeneralDB;Trusted_Connection=True;Encrypt=False;' Microsoft.EntityFrameworkCore.SqlServer
