3c4ab3f123c3fadc27f31507a29596b483111912
VyacheslavZinch <89160613+VyacheslavZinch@users.noreply.github.com>
2023-02-12 01:18:42 +0200

    2/12/2023

---
+++ Server/Database/DatabaseMethods.cs
@@ -0,0 +1,45 @@
+﻿using SimplePlatform.Classes;
+using System.Text;
+using System.Text.Json;
+
+namespace Server.Database
+{
+    public class DatabaseMethods
+    {
+        public static string GetInfoAboutUser(string UID)
+        {
+            StringBuilder sb = new StringBuilder();
+            using (GeneralDbContext db = new GeneralDbContext())
+            {
+                User usr = new User();
+                usr.UID = UID;
+#pragma warning disable CS8601 // Possible null reference assignment.
+
+
+                usr.Name = (from p in db.UserInfos
+                            where p.Uid == UID
+                            select p.Name).ToString();
+
+                usr.Surname = (from p in db.UserInfos
+                            where p.Uid == UID
+                            select p.Surname).ToString();
+
+                usr.Email = (from p in db.UserInfos
+                            where p.Uid == UID
+                            select p.Email).ToString();
+
+                usr.DateOfRegistration = (from p in db.UserInfos
+                            where p.Uid == UID
+                            select p.Dateofregistration).ToString();
+
+                usr.Password = String.Empty;
+
+#pragma warning restore CS8601 // Possible null reference assignment.
+
+            }
+
+            string result = JsonSerializer.Serialize(sb);
+            return result;
+        }
+    }
+}
---
+++ Server/User.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Server;
+
+namespace SimplePlatform.Classes
+{
+    internal class User
+    {
+        public string Name;
+        public string? Surname;
+        public string Email;
+        public string DateOfRegistration;
+        public string Password;
+        public string UID;
+
+    }
+}
